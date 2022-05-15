d81690b50a657ecbfc1111446f2774fcc8621883
eoreordl <59041490+eoreordl@users.noreply.github.com>
2022-05-15 11:27:08 +0900

    Update site.js

--- myCode/html/js/site.js
+++ myCode/html/js/site.js
@@ -1,7 +1,12 @@
 const search = document.querySelector("#search");
 const btn = document.querySelector("#button");
-function searching() {
-    console.log("hello");
+
+
+function searching(evnet) {
+    event.preventDefault();
+    const searchWording = "http://www.youtube.com/results?search_query=" + search.value;
+    search.value = "";
+    window.open(searchWording);
 }
 
 btn.addEventListener("click", searching);
